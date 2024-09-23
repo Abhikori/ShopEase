b5de2762cf51b10af7f1bc638298b71f53cfe58e
Abhikori <abhishekkori7902@gmail.com>
2024-09-23 20:21:34 +0530

    nav-content component

--- angular-frontend/src/app/Module/shared/components/navbar/nav-content/nav-content.component.ts
+++ angular-frontend/src/app/Module/shared/components/navbar/nav-content/nav-content.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { navigation } from './nav-content';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav-content',
@@ -13,7 +14,14 @@ export class NavContentComponent {
   category:any
   @Input() selectedSection:any
 
+  constructor(private router:Router) { } 
+
   ngOnInit(){
     this.category=navigation;
   }
+
+  handleNavigate=(path:any)=>{
+    this.router.navigate([path])
+  }
+
 }
