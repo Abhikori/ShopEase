4eea66460c5d982d558bcc7c5278cf9ace6c0b6a
Abhikori <abhishekkori7902@gmail.com>
2024-09-23 20:22:17 +0530

    app config

--- angular-frontend/src/app/app.config.ts
+++ angular-frontend/src/app/app.config.ts
@@ -7,7 +7,23 @@ import { provideStore } from '@ngrx/store';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { authReducer } from './State/Auth/auth.reducer';
 import { userReducer } from './State/User/user.reducer';
+import { productReducer } from './State/Product/product.reducer';
+import { cartReducer } from './State/Cart/cart.reducer';
+import { orderReducer } from './State/Order/order.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(), provideStore({auth:authReducer, user:userReducer}), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    provideStore({
+      auth: authReducer,
+      user: userReducer,
+      product: productReducer,
+      cart: cartReducer,
+      order: orderReducer,
+    }),
+    provideAnimationsAsync(),
+  ],
 };
