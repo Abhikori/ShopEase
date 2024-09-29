8970a26f757e1f4032f2fa6ab08d9674e2fdc7ee
Abhikori <abhishekkori7902@gmail.com>
2024-09-29 20:20:20 +0530

    cart component

--- angular-frontend/src/app/Module/feature/components/cart/cart.component.ts
+++ angular-frontend/src/app/Module/feature/components/cart/cart.component.ts
@@ -4,6 +4,9 @@ import { MatDividerModule } from '@angular/material/divider';
 import { CartItemComponent } from "../../../shared/components/cart-item/cart-item.component";
 import { Router } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { CartService } from '../../../../State/Cart/cart.service';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../../../Models/AppState';
 
 @Component({
   selector: 'app-cart',
@@ -14,9 +17,19 @@ import { MatButtonModule } from '@angular/material/button';
 })
 export class CartComponent {
 
-  cart=[1,1,1]
+  cart=[1,1,1];
+  cartItems:any;
 
-  constructor(private router:Router){}
+  constructor(private router:Router,private cartService:CartService,private store:Store<AppState>){}
+
+  ngOnInit(){
+    this.cartService.getCart()
+
+    this.store.pipe(select((store) => store.cart)).subscribe((cart) => {
+      this.cartItems = cart.cartItems;
+      console.log(this.cartItems);
+    });
+  }
 
   naviageToCheckout(){
     this.router.navigate(["checkout"])
