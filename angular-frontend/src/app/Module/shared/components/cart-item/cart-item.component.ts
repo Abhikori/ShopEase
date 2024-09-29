581c4deae519a2f51b9e37d81637d3c01747f9c4
Abhikori <abhishekkori7902@gmail.com>
2024-09-29 20:21:39 +0530

    cart-item component

--- angular-frontend/src/app/Module/shared/components/cart-item/cart-item.component.ts
+++ angular-frontend/src/app/Module/shared/components/cart-item/cart-item.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { CartService } from '../../../../State/Cart/cart.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -12,14 +13,18 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class CartItemComponent {
 
+  @Input() cartItem:any;
   @Input() showButton:any;
+
+  constructor(private cartService:CartService) { }
 
   updateCartItem(number: any){
     console.log(number);
+    this.cartService.updateCartItem({cartItemId:this.cartItem.id, data:{quantity:number+this.cartItem.quantity}});
   }
 
   removeCartItem(){
-    
+    this.cartService.removeCartItem(this.cartItem.id);
   }
 
 }
