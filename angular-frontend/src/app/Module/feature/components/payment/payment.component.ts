e784c5f202b9eb7b204d6b1cdca4abb362e26b4c
Abhikori <abhishekkori7902@gmail.com>
2024-10-02 21:00:50 +0530

    payment component

--- angular-frontend/src/app/Module/feature/components/payment/payment.component.ts
+++ angular-frontend/src/app/Module/feature/components/payment/payment.component.ts
@@ -1,35 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDividerModule } from '@angular/material/divider';
-import { AddressCardComponent } from "../../../shared/components/address-card/address-card.component";
-import { CartItemComponent } from "../../../shared/components/cart-item/cart-item.component";
+import { AddressCardComponent } from '../../../shared/components/address-card/address-card.component';
+import { CartItemComponent } from '../../../shared/components/cart-item/cart-item.component';
 import { ActivatedRoute } from '@angular/router';
 import { OrderService } from '../../../../State/Order/order.service';
 import { select, Store } from '@ngrx/store';
 import { AppState } from '../../../../Models/AppState';
+import { PaymentService } from '../../../../State/Payment/payment.service';
 
 @Component({
   selector: 'app-payment',
   standalone: true,
-  imports: [CommonModule,MatDividerModule, AddressCardComponent, CartItemComponent],
+  imports: [
+    CommonModule,
+    MatDividerModule,
+    AddressCardComponent,
+    CartItemComponent,
+  ],
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.scss'
+  styleUrl: './payment.component.scss',
 })
 export class PaymentComponent {
-  products=[1,1,1,1,1]
-  order:any
+  products = [1, 1, 1, 1, 1];
+  order: any;
 
-  constructor(private activatedRoute:ActivatedRoute, private orderService:OrderService, private store:Store<AppState>){}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private orderService: OrderService,
+    private store: Store<AppState>,
+    private paymentService: PaymentService
+  ) {}
 
-  ngOnInit(){
-    let id=this.activatedRoute.snapshot.paramMap.get("id")
+  ngOnInit() {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
 
-    if(id)
-      this.orderService.getOrderById(id)
+    if (id) this.orderService.getOrderById(id);
 
-    this.store.pipe(select(store=>store.order)).subscribe((order)=>{
-      this.order=order.order
-    })
+    this.store.pipe(select((store) => store.order)).subscribe((order) => {
+      this.order = order.order;
+    });
+  }
 
+  redirectToPayment = () => {
+    if(this.order.id)
+      this.paymentService.createPayment(this.order.id);
   }
 }
