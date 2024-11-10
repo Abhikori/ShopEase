eb1c5e2c83a69120604e8207e5e060b6c2fce3a8
Abhikori <abhishekkori7902@gmail.com>
2024-11-10 21:11:07 +0530

    order service

---
+++ springboot-backend/src/main/java/com/shopease/service/OrderService.java
@@ -0,0 +1,33 @@
+package com.shopease.service;
+
+import java.util.List;
+
+import com.shopease.exception.OrderException;
+import com.shopease.model.Address;
+import com.shopease.model.Order;
+import com.shopease.model.User;
+
+public interface OrderService {
+
+    public Order createOrder(User user, Address address);
+
+    public Order findOrderById(Long orderId) throws OrderException;
+
+    public List<Order> usersOrderHistory(Long userId);
+
+    public Order placedOrder(Long orderId) throws OrderException;
+
+    public Order confirmedOrder(Long orderId) throws OrderException;
+
+    public Order shippedOrder(Long orderId) throws OrderException;
+
+    public Order deliveredOrder(Long orderId) throws OrderException;
+
+    public Order cancledOrder(Long orderId) throws OrderException;
+
+    public List<Order> getAllOrders();
+
+    public void deleteOrder(Long orderId) throws OrderException;
+
+
+}
